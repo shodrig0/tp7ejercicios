02be1be5946e218a22b0bb7c1b152a9f5408473b
shodrig0 <138055427+shodrig0@users.noreply.github.com>
2023-11-12 16:09:11 -0300

    finalizar este putno en facultad

---
+++ 6.php
@@ -0,0 +1,38 @@
+<?php
+
+//Una función buscarPrimerMenorA, cuyos dos parámetros de entrada formal sean: un arreglo Multidimensional de mascotas y un entero que represente la edad. La función debe retornar -1 si no existe una mascota menor a la edad ingresada. Si existe una mascota menor a dicha edad, la función debe retornar la primer mascota que cumpla con dicha condición. (Implementar un recorrido Parcial).
+function buscarPrimerMenorA($misMascotas, $edad)
+{
+    $n = count($misMascotas);
+    $i = 0;
+    while ($i < $n && $misMascotas[$i]["edad"] >= $edad) {
+        $i++; //para seguir evaluando la condicion
+    }
+    if ($i < $n) {
+        $edadCondicion = $misMascotas[$i];
+    } else {
+        $edadCondicion = -1;
+    }
+    return $edadCondicion;
+}
+
+$mascota1 = ["nombre" => "Gonzo", "edad" => 9, "tipo" => "perro"];
+$mascota2 = ["nombre" => "Peggy", "edad" => 3, "tipo" => "puerco"];
+$mascota3 = ["nombre" => "Harry", "edad" => 4, "tipo" => "hamster"];
+$mascota4 = ["nombre" => "Bobby", "edad" => 10, "tipo" => "gato"];
+
+$misMascotas[0] = $mascota1;
+$misMascotas[1] = $mascota2;
+$misMascotas[2] = $mascota3;
+$misMascotas[3] = $mascota4;
+
+$edadMascota = 2;
+
+$registroEdad = buscarPrimerMenorA($misMascotas, $edadMascota);
+
+if ($registroEdad == -1) {
+    echo -1;
+} else {
+    print_r($registroEdad);
+}
+//preguntar
